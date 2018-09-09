a6380a7cf81bab030873ae7a927e0dacf02ab419
Dennis Onder <webdev.dennisonder@gmail.com>
2018-09-09 14:07:24 +0200

    Added Post endpoint for adding new items to the array, and basic validation

--- express.js
+++ express.js
@@ -2,7 +2,22 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const courses = ['Course 1', 'Course 2', 'Course 2'];
+app.use(express.json());
+
+const courses = [
+  {
+    id: 1,
+    name: 'Course 1'
+  },
+  {
+    id: 2,
+    name: 'Course 2'
+  },
+  {
+    id: 3,
+    name: 'Course 3'
+  },
+];
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -13,7 +28,23 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.get('/api/courses/:id', (req, res) => {
-  res.send(courses[req.params.id - 1]);
+  const course = courses.find(course => course.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).json({ notFound: 'Course not found.' });
+  }
+  res.send(course);  
+});
+
+app.post('/api/courses', (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    res.status(400).send('Invalid name.');
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  }
+  courses.push(course);
+  res.send(course);
 });
 
 app.listen(port, () => console.log('Listening on port:' + port));
