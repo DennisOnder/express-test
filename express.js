97b74c2129d9e8c179af45234acd923490f5a46d
Dennis Onder <webdev.dennisonder@gmail.com>
2018-09-09 13:37:21 +0200

    Added more endpoints

--- express.js
+++ express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+
 const courses = ['Course 1', 'Course 2', 'Course 2'];
 
 app.get('/', (req, res) => {
@@ -9,6 +10,10 @@ app.get('/', (req, res) => {
 
 app.get('/api/courses', (req, res) => {
   res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+  res.send(courses[req.params.id - 1]);
 });
 
 app.listen(port, () => console.log('Listening on port:' + port));
