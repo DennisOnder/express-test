363543c8a7bfdafd8e0c62155de2fa607b0eaf47
Dennis Onder <webdev.dennisonder@gmail.com>
2018-09-09 13:32:29 +0200

    Added Nodemon as a dev dependency, and added additional endpoints

--- express.js
+++ express.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
+const courses = ['Course 1', 'Course 2', 'Course 2'];
 
 app.get('/', (req, res) => {
   res.send('Hello World');
-})
+});
 
-app.listen(5000);
+app.get('/api/courses', (req, res) => {
+  res.send(courses);
+});
+
+app.listen(port, () => console.log('Listening on port:' + port));
